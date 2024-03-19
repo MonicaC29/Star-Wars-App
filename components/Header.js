9c3eab6822fb59642d1f333204750de8ae0a41bc
MONICA CARTA <148866377+MonicaC29@users.noreply.github.com>
2024-03-19 23:45:31 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className={styles.header}>
       <div>
         <div className={styles.title}>~~~ Star Wars ~~~</div>
-        <image src="/starWars.png" alt="Star Wars Logo" className={styles.logo} />
+        <Image src="/starWars.png" alt="Star Wars Logo" className={styles.logo} />
       </div>
     </header>
   );
